a53b8b1d0d18bbe9ed980af2707b43a3433f6443
Michelle Totherow <tym4me13@gmail.com>
2023-03-29 07:38:03 -0500

    first commit

---
+++ list.js
@@ -0,0 +1,243 @@
+// A
+// Accelerated mobile pages:
+
+// Accelerated Mobile Pages (AMPs) are stripped-down web pages with minimal HTML intended to optimize search results on mobile devices. Because of their minimal nature, AMPs can offer faster load times and less data usage, improving visibility on mobile devices.
+
+ 
+
+// Adaptive design:
+// A way to build a website with several different layouts suited for different screen sizes.
+
+// Algorithm:
+// A set of steps for carrying out specific tasks. When an algorithm is created, developers document all the steps needed to solve a problem and what each step involves.
+
+// API (application program interface):
+// How computers and applications communicate with each other.
+
+// Application (app):
+// A type of software that allows the user to perform different tasks.
+
+// Attribute: 
+// Information about elements of a component in your website design/build
+
+// B
+// B2B web development:
+// The process of designing and creating a website that serves as an online space for your business-to-business company. Good B2B web development will help consumers learn more about your business and browse and purchase products more efficiently.
+
+// Back-end:
+// Everything going on behind the scenes, powering what happens at the front-end. There are four main components of a back-end development software stack: The server, the database, the operating system, and the software.
+
+// Bandwidth:
+// Bandwidth refers to the maximum amount of data that can be transmitted between the Internet and your site in a given amount of time. Having a larger bandwidth allows you to move data faster and can improve user experience.
+
+ 
+
+// Bootstrap:
+// A free, open-source, front-end framework for designing websites and web apps, developed by Mark Otto and Jacob Thornton at Twitter to encourage consistency across internal tools.
+
+// Browser:
+// Program used to access the Internet and display web pages. When you type a web address or URL into a browser, it sends out a series of requests. It gathers different elements that make up the webpage, like images, ads, and content to display the page.
+
+// Bug:
+// An error or flaw that prevents a website or app from running correctly.
+
+// C
+// Cache:
+// The storage of certain elements to create faster load times for repeat website visitors. If a change is made to a website, users will need to clear their browser’s cache to see it.
+
+// Classes:
+// An identifier in CSS for specifying what you want to target with styling. In other programming languages, classes are broadly used as a blueprint to create something.
+
+// CMS (content management system):
+// The program you use to create and maintain your website’s content. A commonly used CMS is WordPress.
+
+// Code:
+// What web developers write using programming languages.
+
+// Conversion:
+// Goals you have for things on your websites, such as email signups and downloads.
+
+// Cookies:
+// Data sent by an Internet server to a browser. Whenever the browser accesses the same server, it sends that data back to track how often it accesses the server.
+
+// Crawl:
+// Occurs when search engines send bots to your website to find out which pages exist and don’t exist to determine what content should be displayed or removed in search results.
+
+// CRM (customer relationship management):
+// Software and applications used to gather, analyze, and maintain information about customers, donors, and prospects.
+
+// CSS (cascading style sheet):
+// Code that tells browsers how to display a webpage to users. This programming formats fonts, colors, and other visual elements.
+
+// CTA (call to action):
+// Buttons on your website to drive specific conversions or goals, like a newsletter signup or a contact form.
+
+// D
+// Data structures:
+// A way to organize and store data for maximum efficiency. These make it easy to find, access, sort, insert, and delete data. Types include linked lists, stacks, queues, and sets.
+
+// Debugging:
+// The process of identifying and dealing with bugs.
+
+// Deployment:
+//  When a website or app goes live after development and testing.
+
+// DevOps (development operations): 
+// System that helps to keep development, IT operations, and quality assurance departments on the same page to make for better end-products and collaborations.
+
+// Domain:
+// The address for a website as entered into the browser.
+
+// Documentation:
+// Essentially the central point of reference for anyone involved in managing, maintaining, or using a website or piece of software. It may provide information on requirements, architecture and design, technical properties, data for the user, or marketing.
+
+// E
+// Ecommerce web development:
+// It is the process of creating a website that will be used to sell or buy products or services online. This can help to generate more revenue and conversions for your business.
+
+// Entreprise web development:
+// It is the process of creating and designing a website used by mid to large (enterprise) level businesses. This involves choosing the best structure for your site to deliver the best user experience.
+
+// F
+// Favicon:
+// Short for “favorite icon”; the icon that appears in your website’s browser tab.
+
+// Fields:
+// The most basic building blocks for data collection. They’re storage units that website visitors use to enter their names, email addresses, notes, etc.
+
+// Firewall:
+// System to protect a secure network from an unsecure network.
+
+// Framework:
+// Suite of programs used in website or software development lays the groundwork for the type of programming language used for your website or app development.
+
+// Front-end:
+// The part of the website or app that the user sees.
+
+// FTP (file transfer protocol):
+// Method of exchanging files from one computer to another.
+
+// Full-stack:
+// Full-stack developers work across all three layers: The front-end, back-end, and database.
+
+// G
+// GUI (graphical user interface):
+// The image of how a website is laid out and meant to be interacted with.
+
+// H
+// HTML (hypertext markup language):
+// Describes how an Internet browser should display a document as a webpage. It consists of tags, elements, and attributes.
+
+// HTTP (hypertext transfer protocol):
+// Used to transfer data across the Internet by sending data from an HTTP server program to an HTTP client program.
+
+// HTTPS:
+
+// HTTPS is an upgraded version of the network protocol standard HTTP. With HTTPS, websites create a secure online experience by encrypting data, like names, addresses, and credit card numbers, and ensuring its safe transfer to the site’s server.
+
+// I
+// Information architecture: 
+// The practice of organizing complex information clearly and logically. This means creating a user-friendly structure that makes it easy for users to navigate through websites and apps.
+
+// J
+// Java:
+// A popular programming language that is platform-independent and can be run on any device regardless of what operating system is used.
+
+// JavaScript:
+// Primarily used as a client-side language, embedded into the HTML of a page and sent to a browser. Web developers use this to add automation, animations, and interactivity to web pages.
+
+// jQuery:
+// A JavaScript library that simplifies the task of using JavaScript on your website by condensing several lines of JavaScript code into a single line.
+
+// M
+// Meta tag:
+// Additional information on web pages or elements, like the way a piece of content should display in Google search results, the credit for an image, or the main keywords associated with a plugin.
+
+// Minification:
+// The process of minimizing code and markup to reduce the file size.
+
+// Mobile-first:
+// An approach to web design and development that prioritizes mobile devices. The website is built first for a small screen, then for a desktop.
+
+// MVP (minimum viable product):
+// the most pared-down version of a product that can be released to the market. When adopting this approach, developers will focus on the core features and functions that are essential. Once the product is released and user feedback is gathered, they will continue to build the complete set of features.
+
+// MySQL:
+// The world’s most popular open-source database management system.
+
+// O
+// OOTB (out of the box):
+// Ready-made, plug-and-play options for features and functions that you can download and install without the need to customize or configure them.
+
+// Opening/closing tags:
+// Angle Brackets (< >) that bookend an HTML element to help build a web page structure. Closing tags include a forward slash (</>).
+
+// Operating system:
+// A type of software that enables the user to run applications on a device.
+
+// P
+// Page template:
+// The layout for a webpage. Pages that have similar structures share the same template.
+
+// PHP (hypertext preprocessor):
+
+// A server-side scripting language used to generate dynamic page content, send and receive cookies, control user access, and encrypt data.
+
+// Plugin:
+// An extension that adds extra functionality to existing software.
+
+// Property:
+// Characteristics that are dictated by CSS, such as color schemes and fonts.
+
+// R
+// Redirects:
+// Automatic forwards from one URL to another, usually from an old website URL to the same page on a new website.
+
+// Registrar:
+// Company used to register domains.
+
+// Resolution:
+// How large an image or graphic can be rendered on a display.
+
+// Responsive design:
+// Websites that accommodate the screen on which they’re being viewed.
+
+// S
+// SAAS platforms:
+// “Software as a service,” the most basic cloud platform that allows users to share files and collaborate on projects through their browser.
+
+// Server:
+// Computers running software that allows users to access your website and houses the hosting.
+
+// Sitemap:
+// An outline of all pages on a website organized in hierarchical order.
+
+// Slider:
+// Carousel of images usually featured on a site’s homepage that rotates, highlighting different photos, links, and content.
+
+// Software stack:
+// Bundles of software that work together to make up the back-end of a website.
+
+// SSL (secure sockets layer):
+// A standard security protocol for ensuring that all data transmitted between the web server and the browser remains encrypted.
+
+// T
+// Text editor:
+// A type of software used to write plain, unformatted text.
+
+// U
+// UI (user interface):
+// Visual elements that go into a website or app.
+
+// UX (user experience):
+// A user’s interaction focused on how satisfying and successful the experience is.
+
+// W
+// Web app:
+// Connect via the Internet browser and adapt to whichever device you’re viewing them on.
+
+// Widgets:
+// Applications that allow for specific interactive functions to be performed on a website.
+
+// Wireframe:
+// The bare-bones structure of a website.
